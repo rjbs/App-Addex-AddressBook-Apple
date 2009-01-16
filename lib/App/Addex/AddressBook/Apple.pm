7b7616451a29b35f72997c09b6c7115a78840a88
Ricardo SIGNES <rjbs@cpan.org>
2009-01-16 09:17:03 -0500

    prep new release

--- lib/App/Addex/AddressBook/Apple.pm
+++ lib/App/Addex/AddressBook/Apple.pm
@@ -16,11 +16,11 @@ App::Addex::AddressBook::Apple - use Apple Address Book as the addex source
 
 =head1 VERSION
 
-version 0.013
+version 0.014
 
 =cut
 
-our $VERSION = '0.013';
+our $VERSION = '0.014';
 
 =head1 SYNOPSIS
 
